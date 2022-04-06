52d95dff937f83e5198cef75488646470b52a50c
Yvan Tankeu <yvantankeu@yahoo.fr>
2022-04-06 10:01:21 -0400

    envoie le decompte du nombre de bascule sur thingsboard

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+/*
+  Titre      : Pluviometrie
+  Auteur     : Crepin Fouelefack AND Yvan Tankeu
+  Date       : 06/04/2022
+  Description: Envoie le nombre de bascule du à une quantité de pluie provenant du auget
+  Version    : 0.0.1
+*/
+
+#include <Arduino.h>
+#include <Arduino.h>
+#include "WIFIConnector_MKR1000.h"
+#include "MQTTConnector.h"
+
+const int PinBouton = 1;
+volatile int Compteur = 0;
+
+int PreviousCounter = 0;
+
+void compteurBascule()
+{
+  Compteur++;
+}
+
+void setup()
+{
+
+  wifiConnect(); // Branchement au réseau WIFI
+  MQTTConnect(); // Branchement au broker MQTT
+
+  Serial.begin(9600);
+  pinMode(PinBouton, INPUT);
+  attachInterrupt(digitalPinToInterrupt(PinBouton), compteurBascule, FALLING);
+}
+
+void loop()
+{
+  if (Compteur != PreviousCounter)
+  {
+    Serial.println(Compteur);
+    PreviousCounter = Compteur;
+
+    // envoie le décompte
+    appendPayload("Compteur", Compteur);
+    sendPayload();
+
+    Compteur = 0;
+  }
+
+}
