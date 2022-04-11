3e6b93f0d75784ef31bdd6e20e721f2e0d41f5da
Yvan Tankeu <yvantankeu@yahoo.fr>
2022-04-10 21:45:33 -0400

    envoie le decompte du nombre de bascule sur thingsboard apres chaque 1 min prev

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,23 @@
   Version    : 0.0.1
 */
 
+//librairie pour le framework
 #include <Arduino.h>
+
+//Librairie de branchement
 #include "WIFIConnector_MKR1000.h"
 #include "MQTTConnector.h"
 
-const int PinReedSwitch = 1;
-volatile int Compteur = 0;
+// Definition des pin
+const int PinReedSwitch = 1; // Reedswitch pour le decompteur
+volatile int Compteur = 0; // Compteur de basculement de l'auget
 
+// Variables de gestions du temps
 unsigned int tempsActuel = 0;
 unsigned int tempsPasse = 0; 
 unsigned int tempsNecessaire = 60000; // 1 min
 
-
-int PreviousCounter = 0;
-
+//Interrupt pour compter le nombre de bascule
 void compteurBascule()
 {
   Compteur++;
@@ -27,24 +30,22 @@ void compteurBascule()
 
 void setup()
 {
+  //Branchement au reseau
   wifiConnect(); // Branchement au réseau WIFI
-  MQTTConnect(); // Branchement au broker MQTT
+  MQTTConnect(); // Branchement au broker MQTT à Thingsboard
 
   Serial.begin(9600);
+  //Definition des pinMode
   pinMode(PinReedSwitch, INPUT);
+
+  // Lien interrupt pour compter les bascules à chaque changement d'etat du haut vers le bas
   attachInterrupt(digitalPinToInterrupt(PinReedSwitch), compteurBascule, FALLING);
 }
 
 void loop()
 {
-  
+  // Sauvegarde du temps en cours
   tempsActuel = millis();
-
-  if (Compteur != PreviousCounter)
-  {
-    Serial.println(Compteur);
-    PreviousCounter = Compteur;
-  }
 
   //Envoie du decompte sur thingsboard  après chaque 01 min
   if ((tempsActuel - tempsPasse) >  tempsNecessaire){
